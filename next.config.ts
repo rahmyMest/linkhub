4c778e5f6afb898efb4fdf161a1d90ebd4d7610a
Rahmat Akintola <rahmat@meltwater.org>
2025-10-29 23:08:03 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'opengraph.githubassets.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
